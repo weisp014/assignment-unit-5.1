c32535414e19df0baddd34924d0e869e6baf0b4d
Ryan Weispfenning <weisp014@gmail.com>
2023-02-15 19:27:58 -0600

    created object me

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,6 +10,11 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'Ryan',
+  lastName: 'Weispfenning',
+  hasSiblings: true,
+  shoeCount: 2,
+  favThreeFoods: ['tacos', 'pizza', 'curry']
 };
 console.log('A little about me:', me);
 
